e2c27e0bd915cbcf766179384c8725056a2a6a11
LustBK <bankskorey18@gmail.com>
2025-08-01 15:45:11 -0400

    Update main.js

--- main.js
+++ main.js
@@ -26,3 +26,39 @@ const speedTest = new FastSpeedtest({
 
 const realTimeSpeed = await speedTest.getSpeed();
 console.log(`Real-time download speed: ${realTimeSpeed} Mbps`)
+
+<div class="progress-bar">
+  <div class="filler"></div>
+</div>
+
+.progress-bar {
+  width: 100%;
+  height: 20px;
+  background-color: #e0e0e0;
+  border-radius: 10px;
+  overflow: hidden;
+}
+
+.filler {
+  width: 0;
+  height: 100%;
+  background-color: #76c7c0;
+  transition: width 0.5s ease-in-out;
+}
+
+function simulateProgressBar() {
+  const filler = document.querySelector('.filler');
+  let width = 0;
+
+  const interval = setInterval(() => {
+    if (width >= 100) {
+      clearInterval(interval);
+    } else {
+      width++;
+      filler.style.width = width + '%';
+    }
+  }, 100);
+}
+
+simulateProgressBar();
+
